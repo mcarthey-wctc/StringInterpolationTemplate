115303c7754b8bd886e02c32afbbe905ec1cec60
Mark McArthey <mcarthey@gmail.com>
2024-02-03 19:52:19 -0600

    Added instructional comments to the service

--- StringInterpolationTemplate/Services/StringInterpolationService.cs
+++ StringInterpolationTemplate/Services/StringInterpolationService.cs
@@ -16,7 +16,7 @@ public class StringInterpolationService : IStringInterpolationService
         _logger.Log(LogLevel.Information, "Executing the StringInterpolationService");
     }
 
-    //1. January 22, 2019 (right aligned in a 40 character field)
+    // 1. January 22, 2019 (right aligned in a 40 character field)
     public string Number01()
     {
         var date = _date.Now.ToString("MMMM dd, yyyy");
@@ -26,55 +26,63 @@ public class StringInterpolationService : IStringInterpolationService
         return answer;
     }
 
+    // 2. 2019.01.22
     public string Number02()
     {
         throw new NotImplementedException();
     }
 
+    // 3. Day 22 of January, 2019
     public string Number03()
     {
         throw new NotImplementedException();
     }
 
+    // 4. Year: 2019, Month: 01, Day: 22
     public string Number04()
     {
         throw new NotImplementedException();
     }
 
+    // 5.            Tuesday (10 spaces total, right aligned)
     public string Number05()
     {
         throw new NotImplementedException();
     }
 
+    // 6.     11:01 PM             Tuesday (10 spaces total for each including the day-of-week, both right-aligned)
     public string Number06()
     {
         throw new NotImplementedException();
     }
 
+    // 7. h:11, m:01, s:27
     public string Number07()
     {
         throw new NotImplementedException();
     }
 
+    // 8. 2019.01.22.11.01.27
     public string Number08()
     {
         throw new NotImplementedException();
     }
 
+    // Output Math.PI as currency
     public string Number09()
     {
         throw new NotImplementedException();
     }
 
+    // Output Math.PI as right-aligned (10 spaces), number with 3 decimal places
     public string Number10()
     {
         throw new NotImplementedException();
     }
 
+    // Output the year (2019) as a hexidecimal value
     public string Number11()
     {
         throw new NotImplementedException();
     }
-
-    //2.2019.01.22
 }
